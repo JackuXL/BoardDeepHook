741d46c5998047098963018c72c33b65800f04ee
JackuXL <JackuXL2019@gmail.com>
2022-06-25 13:29:02 +0800

    Initial commit

---
+++ app/src/main/java/cn/jackuxl/boarddeephook/MainActivity.kt
@@ -0,0 +1,15 @@
+package cn.jackuxl.boarddeephook
+
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
